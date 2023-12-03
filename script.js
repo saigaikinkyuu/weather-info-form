536cda92426f9671475fba187da26b12b8e0443f
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-12-03 17:30:53 +0900

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+// Webカメラの起動
+const video = document.getElementById('video');
+let contentWidth;
+let contentHeight;
+
+const media = navigator.mediaDevices.getUserMedia({ audio: false, video: {width:640, height:480} })
+   .then((stream) => {
+      video.srcObject = stream;
+      video.onloadeddata = () => {
+         video.play();
+         contentWidth = video.clientWidth;
+         contentHeight = video.clientHeight;
+         canvasUpdate(); // 次で記述
+         checkImage(); // 次で記述
+      }
+   }).catch((e) => {
+      console.log(e);
+   });
+// カメラ映像のキャンバス表示
+const cvs = document.getElementById('camera-canvas');
+const ctx = cvs.getContext('2d');
+const canvasUpdate = () => {
+   cvs.width = contentWidth;
+   cvs.height = contentHeight;
+   ctx.drawImage(video, 0, 0, contentWidth, contentHeight);
+   requestAnimationFrame(canvasUpdate);
+}
+const code = jsQR(imageData, width, height, options?);
+
+if (code) {
+  console.log("Found QR code", code);
+}
+// QRコードの検出
+const rectCvs = document.getElementById('rect-canvas');
+const rectCtx =  rectCvs.getContext('2d');
+const checkImage = () => {
+   // imageDataを作る
+   const imageData = ctx.getImageData(0, 0, contentWidth, contentHeight);
+   // jsQRに渡す
+   const code = jsQR(imageData.data, contentWidth, contentHeight);
+
+   // 検出結果に合わせて処理を実施
+   if (code) {
+      console.log("QRcodeが見つかりました", code);
+      drawRect(code.location);
+   } else {
+      console.log("QRcodeが見つかりません…", code);
+      rectCtx.clearRect(0, 0, contentWidth, contentHeight);
+   }
+   setTimeout(()=>{ checkImage() }, 500);
+}
+// 四辺形の描画
+const drawRect = (location) => {
+   rectCvs.width = contentWidth;
+   rectCvs.height = contentHeight;
+   drawLine(location.topLeftCorner, location.topRightCorner);
+   drawLine(location.topRightCorner, location.bottomRightCorner);
+   drawLine(location.bottomRightCorner, location.bottomLeftCorner);
+   drawLine(location.bottomLeftCorner, location.topLeftCorner)
+}
+
+// 線の描画
+const drawLine = (begin, end) => {
+   rectCtx.lineWidth = 4;
+   rectCtx.strokeStyle = "#F00";
+   rectCtx.beginPath();
+   rectCtx.moveTo(begin.x, begin.y);
+   rectCtx.lineTo(end.x, end.y);
+   rectCtx.stroke();
+}
