33686709a816f557ef40dd55bec892071a6dcdf6
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-12-03 17:20:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,25 +12,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 video.srcObject = stream;
                 video.play();
 
-                const canvasElement = document.createElement('canvas');
-                const canvas = canvasElement.getContext('2d');
-                canvasElement.width = video.videoWidth;
-                canvasElement.height = video.videoHeight;
+                // カメラのメタデータが読み込まれた後にcanvasを生成
+                video.addEventListener('loadedmetadata', () => {
+                    const canvasElement = document.createElement('canvas');
+                    const canvas = canvasElement.getContext('2d');
+                    canvasElement.width = video.videoWidth;
+                    canvasElement.height = video.videoHeight;
 
-                requestAnimationFrame(tick);
+                    document.body.appendChild(canvasElement);
 
-                function tick() {
-                    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-                    const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-                    const code = jsQR(imageData.data, imageData.width, imageData.height);
+                    requestAnimationFrame(tick);
 
-                    if (code) {
-                        qrCodeInput.value = code.data;
-                        stopQRCodeScanner(stream, video);
-                    } else {
-                        requestAnimationFrame(tick);
+                    function tick() {
+                        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+                        const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                        const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+                        if (code) {
+                            qrCodeInput.value = code.data;
+                            stopQRCodeScanner(stream, video, canvasElement);
+                        } else {
+                            requestAnimationFrame(tick);
+                        }
                     }
-                }
+                });
             })
             .catch((error) => {
                 console.error('Error accessing the camera:', error);
@@ -38,13 +43,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     // カメラとビデオ要素を停止
-    function stopQRCodeScanner(stream, video) {
+    function stopQRCodeScanner(stream, video, canvasElement) {
         if (video.srcObject) {
             const tracks = video.srcObject.getTracks();
             tracks.forEach(track => track.stop());
         }
 
         document.body.removeChild(video);
+        document.body.removeChild(canvasElement);
     }
 
     // QRコードスキャナーの開始
