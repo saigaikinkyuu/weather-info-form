34ed227b9a76ccba7008a3b1ee46e9eb7bae27f9
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-12-03 16:40:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,32 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const barcodeForm = document.getElementById('barcodeForm');
-    const barcodeInput = document.getElementById('barcodeInput');
+    const qrCodeForm = document.getElementById('qrCodeForm');
+    const qrCodeInput = document.getElementById('qrCodeInput');
 
-    // カメラからバーコードを読み取る関数
-    function startBarcodeScanner() {
-        const constraints = { video: true };
+    // カメラからQRコードを読み取る関数
+    function startQRCodeScanner() {
+        const scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then((stream) => {
-                const video = document.createElement('video');
-                document.body.appendChild(video);
-                video.srcObject = stream;
-                video.play();
-
-                // バーコードリーダーライブラリの初期化
-                const barcodeReader = new ZXing.BrowserMultiFormatReader();
-
-                // カメラからのフレームを処理するコールバック
-                function onFrameCallback(video, barcodeReader, barcodeInput) {
-                    const canvas = document.createElement('canvas');
-                    const context = canvas.getContext('2d');
-                    canvas.width = video.videoWidth;
-                    canvas.height = video.videoHeight;
-                    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-                    const barcodeResult = barcodeReader.decodeFromImage(imageData);
-
-                    if (barcodeResult) {
-                        barcodeInput.value = barcodeResult.text;
-                        stopBarcodeScanner(stream, video);
-                    } else {
-                        requestAnimationFrame(() => onFrameCallback(video, barcodeReader, barcodeInput));
-                    }
-                }
-
-                // バーコードスキャナーの開始
-                onFrameCallback(video, barcodeReader, barcodeInput);
-            })
-            .catch((error) => {
-                console.error('Error accessing the camera:', error);
-            });
-    }
-
-    // カメラアクセスの権限を確認してからスキャナーを開始
-    navigator.permissions.query({ name: 'camera' })
-        .then((permissionStatus) => {
-            if (permissionStatus.state === 'granted') {
-                startBarcodeScanner();
-            } else {
-                console.warn('Camera access permission is not granted.');
-            }
+        scanner.addListener('scan', function (content) {
+            qrCodeInput.value = content;
+            stopQRCodeScanner(scanner);
         });
-});
 
-// カメラとビデオ要素を停止
-function stopBarcodeScanner(stream, video) {
-    if (video.srcObject) {
-        const tracks = video.srcObject.getTracks();
-        tracks.forEach(track => track.stop());
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (error) {
+            console.error('Error accessing the camera:', error);
+        });
     }
 
-    document.body.removeChild(video);
-}
+    // カメラとスキャナーを停止
+    function stopQRCodeScanner(scanner) {
+        scanner.stop();
+    }
+
+    // QRコードスキャナーの開始
+    startQRCodeScanner();
+});
