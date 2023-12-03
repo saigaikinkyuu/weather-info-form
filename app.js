02ba062a8d43e325a5fe6a704c3fcf90b75373f3
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2023-12-03 16:16:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
-// app.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    const barcodeForm = document.getElementById('barcodeForm');
+    const barcodeInput = document.getElementById('barcodeInput');
 
-// Quagga.jsの設定
-Quagga.init({
-    inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector("#interactive"),
-    },
-    decoder: {
-        readers: ["ean_reader", "code_128_reader"],
-    },
-}, function (err) {
-    if (err) {
-        console.error(err);
-        return;
+    // カメラからバーコードを読み取る関数
+    function startBarcodeScanner() {
+        const constraints = { video: true };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                const video = document.createElement('video');
+                document.body.appendChild(video);
+                video.srcObject = stream;
+                video.play();
+
+                // バーコードリーダーライブラリの初期化
+                const barcodeReader = new ZXing.BrowserMultiFormatReader();
+
+                // カメラからのフレームを処理するコールバック
+                function onFrameCallback(video, barcodeReader, barcodeInput) {
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.width = video.videoWidth;
+                    canvas.height = video.videoHeight;
+                    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+                    const barcodeResult = barcodeReader.decodeFromImage(imageData);
+
+                    if (barcodeResult) {
+                        barcodeInput.value = barcodeResult.text;
+                        stopBarcodeScanner(stream, video);
+                    } else {
+                        requestAnimationFrame(() => onFrameCallback(video, barcodeReader, barcodeInput));
+                    }
+                }
+
+                // バーコードスキャナーの開始
+                onFrameCallback(video, barcodeReader, barcodeInput);
+            })
+            .catch((error) => {
+                console.error('Error accessing the camera:', error);
+            });
     }
-    console.log("Quagga.js initialized");
 
-    // カメラからのバーコード読み取り開始
-    Quagga.start();
+    // カメラアクセスの権限を確認してからスキャナーを開始
+    navigator.permissions.query({ name: 'camera' })
+        .then((permissionStatus) => {
+            if (permissionStatus.state === 'granted') {
+                startBarcodeScanner();
+            } else {
+                console.warn('Camera access permission is not granted.');
+            }
+        });
 });
 
-// バーコードが読み取られたときの処理
-Quagga.onDetected(function (result) {
-    var code = result.codeResult.code;
+// カメラとビデオ要素を停止
+function stopBarcodeScanner(stream, video) {
+    if (video.srcObject) {
+        const tracks = video.srcObject.getTracks();
+        tracks.forEach(track => track.stop());
+    }
 
-    // 読み取ったバーコードを表示
-    document.getElementById("result").textContent = "バーコード: " + code;
-
-    // フォームにバーコードを自動入力
-    document.getElementById("barcodeInput").value = code;
-});
+    document.body.removeChild(video);
+}
